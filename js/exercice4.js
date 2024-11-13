f0b551dba9aaff4b1a0ad0e0d0ba91e7c9c6a834
Kazk12 <kazuko43KO@gmail.com>
2024-11-13 13:31:34 +0100

    ADD:Exercice 1 2 3 4 JS

---
+++ js/exercice4.js
@@ -0,0 +1,5 @@
+ let Nom = prompt(`Entrez votre Nom ici :`);
+ let Prenom = prompt(`Entrez votre Prénom ici :`);
+ let Ville = prompt(`Entrez votre Ville ici :`);
+
+ window.alert(`Votre Nom : ${Nom} \n Votre Prénom : ${Prenom} \n Votre Ville : ${Ville} `);
---
+++ js/exercice_1_2_3.js
@@ -0,0 +1,25 @@
+
+// Exercice 1
+
+// let message = prompt(`Entrez votre mot ici : `);
+
+// console.log(message);
+
+
+
+// Exercice 2 
+
+
+// let Nom = prompt(`Entrez votre Nom ici :`);
+// let Prenom = prompt(`Entrez votre Prénom ici :`);
+// let Ville = prompt(`Entrez votre Ville ici :`);
+
+// window.alert(`Votre Nom : ${Nom} \n Votre Prénom : ${Prenom} \n Votre Ville : ${Ville} `);
+
+
+// Exercice 3
+
+// let pren = prompt(`Entrez votre prénom ici : `);
+// window.alert(`Bonjour, ${pren}`);
+
+
