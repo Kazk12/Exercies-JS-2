7da240b1aba77966b80f530d0138df75632e0537
Kazk12 <kazuko43KO@gmail.com>
2024-11-13 13:59:51 +0100

    Exercice 5

---
+++ js/exercice5.js
@@ -0,0 +1,8 @@
+// let num1 = prompt(`Entrez votre 1 nombre ici : `);
+// let num2 = prompt(`Entrez votre 2ème nombre ici : `);
+
+// let multiplication = Math.trunc(num1) * Math.trunc(num2);
+
+// window.alert(multiplication);
+
+
