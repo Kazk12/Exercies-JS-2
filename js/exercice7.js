17b44baaf50e541f059470d0893e72655ac2225b
Kazk12 <kazuko43KO@gmail.com>
2024-11-15 09:25:55 +0100

    MODIFY: Exercice7.js

--- js/exercice7.js
+++ js/exercice7.js
@@ -1,6 +1,6 @@
 function valider (){
-    shoeSize = document.getElementById("shoeSize").value;
-    yearOfBirth = document.getElementById("yearOfBirth").value;
+    let shoeSize = document.getElementById("shoeSize").value;
+    let yearOfBirth = document.getElementById("yearOfBirth").value;
     shoeSize = shoeSize * 2;
     shoeSize = shoeSize + 5;
     shoeSize = shoeSize * 50;
